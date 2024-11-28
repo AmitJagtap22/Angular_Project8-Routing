7a36dee1f6facebf73fece7bd043087d1f7b16cc
AmitJ <amitjgtp2@gmail.com>
2024-11-28 22:13:25 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
+import { InvalidComponent } from './invalid/invalid.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:WelcomeComponent
+    },
+    {
+        path:'Technologies',
+        component:TechnologiesComponent
+    },
+    {
+        path:'Books',
+        component:BooksComponent
+    },
+    {
+        path:"**",
+        component:InvalidComponent
+    }
+];
